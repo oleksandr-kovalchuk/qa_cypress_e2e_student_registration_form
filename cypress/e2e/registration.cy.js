67bb8c51e8fbab3dd3e2a1c4fbbc0ab5240ac6a8
Oleksandr Kovalchuk <kov280396@gmail.com>
2025-05-24 15:28:25 +0300

    changed timeout

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,62 +1,101 @@
-/// <reference types='cypress' />
+/// <reference types="cypress" />
 
-describe('Student Registration page', () => {
-  const studentData = {
-    firstName: 'Oleksandr',
-    lastName: 'Petrenko',
-    email: 'oleksandr.petrenko@example.com',
-    mobile: '0987654321',
-    address: 'Вулиця Тестова 456',
+import { faker } from '@faker-js/faker';
+
+describe('Student Registration Page', () => {
+  const student = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    mobile: faker.string.numeric(10),
+    address: faker.location.streetAddress(),
     gender: 'Male',
-    dateOfBirth: '01 January,2000',
+    dateOfBirth: {
+      day: '01',
+      month: 'January',
+      year: '2000',
+      formatted: '01 January,2000'
+    },
     subjects: 'Computer Science',
     hobbies: 'Sports',
     state: 'NCR',
     city: 'Delhi'
   };
 
-  const expectedData = [
-    { label: 'Student Name', value: `${studentData.firstName} ${studentData.lastName}` },
-    { label: 'Student Email', value: studentData.email },
-    { label: 'Gender', value: studentData.gender },
-    { label: 'Mobile', value: studentData.mobile },
-    { label: 'Date of Birth', value: studentData.dateOfBirth },
-    { label: 'Subjects', value: studentData.subjects },
-    { label: 'Hobbies', value: studentData.hobbies },
-    { label: 'Address', value: studentData.address },
-    { label: 'State and City', value: `${studentData.state} ${studentData.city}` }
+  const expectedModalData = [
+    {
+      label: 'Student Name',
+      value: `${student.firstName} ${student.lastName}`
+    },
+    { label: 'Student Email', value: student.email },
+    { label: 'Gender', value: student.gender },
+    { label: 'Mobile', value: student.mobile },
+    { label: 'Date of Birth', value: student.dateOfBirth.formatted },
+    { label: 'Subjects', value: student.subjects },
+    { label: 'Hobbies', value: student.hobbies },
+    { label: 'Address', value: student.address },
+    { label: 'State and City', value: `${student.state} ${student.city}` }
   ];
 
+  const selectors = {
+    firstName: '#firstName',
+    lastName: '#lastName',
+    email: '#userEmail',
+    genderMaleLabel: 'label[for="gender-radio-1"]',
+    mobile: '#userNumber',
+    dateOfBirthInput: '#dateOfBirthInput',
+    monthSelect: '.react-datepicker__month-select',
+    yearSelect: '.react-datepicker__year-select',
+    subjects: '#subjectsInput',
+    hobbiesSportsLabel: 'label[for="hobbies-checkbox-1"]',
+    address: '#currentAddress',
+    stateDropdown: '#state',
+    cityDropdown: '#city',
+    stateOption: '#react-select-3-option-0',
+    cityOption: '#react-select-4-option-0',
+    submit: '#submit',
+    modal: '.modal-content',
+    table: 'table'
+  };
+
+  const selectDateOfBirth = ({ day, month, year }) => {
+    cy.get(selectors.dateOfBirthInput).click();
+    cy.get(selectors.monthSelect).select(month);
+    cy.get(selectors.yearSelect).select(year);
+    cy.get(`.react-datepicker__day--0${day}`)
+      .not('.react-datepicker__day--outside-month')
+      .first()
+      .click();
+  };
+
   beforeEach(() => {
-    cy.visit('https://demoqa.com/automation-practice-form');
+    cy.visit('https://demoqa.com/automation-practice-form', { timeout: 60000 });
   });
 
-  it('should fill the form and verify the modal data', () => {
-    cy.get('#firstName').type(studentData.firstName);
-    cy.get('#lastName').type(studentData.lastName);
-    cy.get('#userEmail').type(studentData.email);
-    cy.get('label[for="gender-radio-1"]').click();
-    cy.get('#userNumber').type(studentData.mobile);
+  it('fills the form and verifies submitted data in modal', () => {
+    cy.get(selectors.firstName).type(student.firstName);
+    cy.get(selectors.lastName).type(student.lastName);
+    cy.get(selectors.email).type(student.email);
+    cy.get(selectors.genderMaleLabel).click();
+    cy.get(selectors.mobile).type(student.mobile);
 
-    cy.get('#dateOfBirthInput').click();
-    cy.get('.react-datepicker__month-select').select('January');
-    cy.get('.react-datepicker__year-select').select('2000');
-    cy.get('.react-datepicker__day--001').first().click();
+    selectDateOfBirth(student.dateOfBirth);
 
-    cy.get('#subjectsInput').type(`${studentData.subjects}{enter}`);
-    cy.get('label[for="hobbies-checkbox-1"]').click();
-    cy.get('#currentAddress').type(studentData.address);
+    cy.get(selectors.subjects).type(`${student.subjects}{enter}`);
+    cy.get(selectors.hobbiesSportsLabel).click();
+    cy.get(selectors.address).type(student.address);
 
-    cy.get('#state').click();
-    cy.contains('#react-select-3-option-0', studentData.state).click();
-    cy.get('#city').click();
-    cy.contains('#react-select-4-option-0', studentData.city).click();
+    cy.get(selectors.stateDropdown).click();
+    cy.contains(selectors.stateOption, student.state).click();
 
-    cy.get('#submit').click();
+    cy.get(selectors.cityDropdown).click();
+    cy.contains(selectors.cityOption, student.city).click();
 
-    cy.get('.modal-content').should('be.visible');
-    expectedData.forEach(({ label, value }) => {
-      cy.get('table').contains(label).next().should('contain', value);
+    cy.get(selectors.submit).click();
+
+    cy.get(selectors.modal).should('be.visible');
+    expectedModalData.forEach(({ label, value }) => {
+      cy.get(selectors.table).contains(label).next().should('contain', value);
     });
   });
 });
